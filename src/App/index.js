9deeff908ad7dbfd365e87300ab644045c288477
ReinTristan <zetaeugene@gmail.com>
2022-08-04 18:47:23 -0500

    Components composition example commit

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppUI } from './AppUI'
 import { TodoProvider } from '../TodoContext'
 
+// function App() {
+// 	return (
+// 		<TodoProvider>
+// 			<AppUI />
+// 		</TodoProvider>
+// 	)
+// }
+
 function App() {
+	const [state, setState] = useState('esta compartido')
 	return (
-		<TodoProvider>
-			<AppUI />
-		</TodoProvider>
+		<>
+			<TodoHeader>
+				<TodoCounter />
+				<TodoSearch />
+			</TodoHeader>
+			<TodoList>
+				<TodoItem state={state} />
+			</TodoList>
+		</>
 	)
+}
+function TodoHeader({ children }) {
+	return <header>{children}</header>
+}
+function TodoList({ children }) {
+	return <section className="TodoList-container">{children}</section>
+}
+function TodoCounter() {
+	return <p>TodoCounter</p>
+}
+
+function TodoSearch() {
+	return <p>TodoSearch</p>
+}
+function TodoItem({ state }) {
+	return <p>TodoItem {state}</p>
 }
 
 export default App
