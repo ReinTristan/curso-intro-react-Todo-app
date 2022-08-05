5c79318fb46c889b6969d2c9da9bd78b5eadca9c
ReinTristan <zetaeugene@gmail.com>
2022-08-04 20:00:08 -0500

    provider transformed into new custom hook useTodos

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,61 @@
-import React, { useState } from 'react'
-import { AppUI } from './AppUI'
-import { TodoProvider } from '../TodoContext'
+import React from 'react'
+import { TodoCounter } from '../TodoCounter'
+import { TodoSearch } from '../TodoSearch'
+import { TodoList } from '../TodoList'
+import { TodoItem } from '../TodoItem'
+import { CreateTodoButton } from '../CreateTodoButton'
+import { Modal } from '../Modal'
+import { TodoForm } from '../TodoForm'
 
+import { TodosLoading } from '../TodosLoading'
+import { TodosError } from '../TodosError'
+import { EmptyTodos } from '../EmptyTodos'
+import { TodoHeader } from '../TodoHeader'
+import { useTodos } from './useTodos'
 function App() {
+	const {
+		loading,
+		error,
+		searchedTodos,
+		completeTodo,
+		deleteTodo,
+		openModal,
+		setOpenModal,
+		totalTodos,
+		completedTodos,
+		searchValue,
+		setSearchValue,
+		addTodo,
+	} = useTodos()
 	return (
-		<TodoProvider>
-			<AppUI />
-		</TodoProvider>
+		<React.Fragment>
+			<TodoHeader>
+				<TodoCounter totalTodos={totalTodos} completedTodos={completedTodos} />
+				<TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+			</TodoHeader>
+
+			<TodoList>
+				{loading && <TodosLoading />}
+				{!loading && error && <TodosError />}
+				{!error && !loading && totalTodos < 1 && <EmptyTodos />}
+
+				{searchedTodos.map((todo) => (
+					<TodoItem
+						key={todo.text}
+						text={todo.text}
+						completed={todo.completed}
+						onComplete={() => completeTodo(todo.text)}
+						onDelete={() => deleteTodo(todo.text)}
+					/>
+				))}
+			</TodoList>
+			{openModal && (
+				<Modal>
+					<TodoForm addTodo={addTodo} setOpenModal={setOpenModal} />
+				</Modal>
+			)}
+			<CreateTodoButton openModal={openModal} setOpenModal={setOpenModal} />
+		</React.Fragment>
 	)
 }
 
--- src/App/useTodos.js
+++ src/App/useTodos.js
@@ -1,9 +1,7 @@
-import React, { createContext, useState } from 'react'
+import { useState } from 'react'
 import { useLocalStorage } from './useLocalStorage'
 
-const TodoContext = createContext()
-
-function TodoProvider(props) {
+function useTodos() {
 	const {
 		item: todos,
 		saveItem: saveTodos,
@@ -50,26 +48,20 @@ function TodoProvider(props) {
 		saveTodos(newTodos)
 	}
 
-	return (
-		<TodoContext.Provider
-			value={{
-				loading,
-				error,
-				totalTodos,
-				completedTodos,
-				searchValue,
-				setSearchValue,
-				searchedTodos,
-				addTodo,
-				completeTodo,
-				deleteTodo,
-				openModal,
-				setOpenModal,
-			}}
-		>
-			{props.children}
-		</TodoContext.Provider>
-	)
+	return {
+		loading,
+		error,
+		totalTodos,
+		completedTodos,
+		searchValue,
+		setSearchValue,
+		searchedTodos,
+		addTodo,
+		completeTodo,
+		deleteTodo,
+		openModal,
+		setOpenModal,
+	}
 }
 
-export { TodoContext, TodoProvider }
+export { useTodos }
