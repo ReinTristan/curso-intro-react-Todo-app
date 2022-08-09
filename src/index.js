a4f0c8574772153b417f0831f2e325e01480c4c7
ReinTristan <zetaeugene@gmail.com>
2022-08-08 21:57:54 -0500

    HOC example in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+// import App from './App'
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+function App(props) {
+	return <h1>{`¡${props.saludo}, ${props.nombre}!`} </h1>
+}
+
+function withSaludo(saludo) {
+	return function WrappedComponetWithSaludo(WrappedComponet) {
+		return function ComponenteDeVerdad(props) {
+			return (
+				<>
+					<WrappedComponet {...props} saludo={saludo} />
+					<p>estamos acompañando al WrappedComponet</p>
+				</>
+			)
+		}
+	}
+}
+
+const AppWithSaludo = withSaludo('wenas')(App)
+
+ReactDOM.render(
+	<AppWithSaludo nombre="juanita" />,
+	// <App/>,
+	document.querySelector('#root')
+)
